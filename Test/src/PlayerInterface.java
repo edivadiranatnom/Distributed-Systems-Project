e05af484b7bc2c7fabc8edd2da05710a78441f3b
bonf93 <simone.bonfante93@gmail.com>
2019-03-07 20:01:32 +0100

    Refactoring

--- Test/src/PlayerInterface.java
+++ Test/src/PlayerInterface.java
@@ -4,7 +4,6 @@ import UnoGame.*;
 
 public interface PlayerInterface extends Remote {
     void getIp (ArrayList<String> ip) throws Exception;
-    void ringComunicaition(int param) throws Exception;
     void electionLeader (String leader) throws Exception;
     Deck testDistribution(Deck deck) throws Exception;
 }
