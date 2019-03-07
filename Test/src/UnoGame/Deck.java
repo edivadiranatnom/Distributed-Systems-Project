fc009cf47c6591b95242de8995c39309cf8a134e
bonf93 <simone.bonfante93@gmail.com>
2019-03-07 10:53:52 +0100

    Shuffle Deck Function

--- Test/src/UnoGame/Deck.java
+++ Test/src/UnoGame/Deck.java
@@ -1,7 +1,7 @@
 package UnoGame;
 
 public class Deck {
-    public static Card[] carddeck = new Card[56];
+    public static Card[] carddeck;
 
     //Le carte vanno da 0 9. Il "salta turno" vale 10, il "cambio giro" vale 11, il "+2" vale 12, il "cambia colore" vale 13, il "+4" vale 14.
     // 13 e 14 non hanno colore.
@@ -9,6 +9,7 @@ public class Deck {
     public static int[] typeCard = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     public static String[] colorCard = new String[]{"rosso", "giallo", "verde", "blu"};
     public Deck () {
+        carddeck = new Card[56];
         int index = -1;
         for (int i = 0; i < typeCard.length; i++) {
             for (int j = 0; j < colorCard.length; j++) {
--- Test/src/UnoGame/UnoGame.java
+++ Test/src/UnoGame/UnoGame.java
@@ -1,13 +1,28 @@
 package UnoGame;
 
+import java.util.*;
+
 public class UnoGame {
-    public static Deck mazzo = new Deck();
+    public static Deck mazzo;
     public UnoGame() {
+        mazzo = new Deck();
     }
     public void stampa () {
         System.out.println("lunghezza del deck"+mazzo.carddeck.length);
         for (int i = 0; i < mazzo.carddeck.length; i++) {
             System.out.println(""+mazzo.carddeck[i].card+", "+mazzo.carddeck[i].color);
         }
+    }
+    public static Card[] shuffle(){
+        Random rgen = new Random();
+
+        for (int i=0; i<mazzo.carddeck.length; i++) {
+            int randomPosition = rgen.nextInt(mazzo.carddeck.length);
+            Card temp = mazzo.carddeck[i];
+            mazzo.carddeck[i] = mazzo.carddeck[randomPosition];
+            mazzo.carddeck[randomPosition] = temp;
+        }
+
+        return mazzo.carddeck;
     }
 }
